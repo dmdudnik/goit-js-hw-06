d3b82aa93e17fdc4cba610ae09cc41e6cf8eff0e
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-01-18 22:06:06 +0200

    добавлены основные файлы

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+//Получить массив объектов пользователей по цвету глаз(поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => {
+  // твой код
+};
+
+console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
---
+++ js/task-7.js
@@ -0,0 +1,7 @@
+//Получить общую сумму баланса(поле balance) всех пользователей.
+
+const calculateTotalBalance = (users) => {
+  // твой код
+};
+
+console.log(calculateTotalBalance(users)); // 20916
