756941d29653fcedb98e93c6b13c982878fbac59
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-01-21 20:59:36 +0200

    Выполнено задание 6

--- js/main.js
+++ js/main.js
@@ -47,30 +47,31 @@ console.log(getInactiveUsers(users)); // [объект Moore Hensley, объек
 // Задание 5
 // Получить пользоваля(не массив) по email(поле email, он уникальный).
 
-const getUserWithEmail = (users, email) =>
-  users
-    .find(function (user) {
-      return user.email === email;
-    })
+// const getUserWithEmail = (users, email) =>
+//   users
+//     .find(function (user) {
+//       return user.email === email;
+//     })
 
-    .map(function (user) {
-      return user.name;
-    });
+//     .map(function (user) {
+//       return user.name;
+//     });
 
-console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
-console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+// console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
+// console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
 
 // Задание 6
 
 // Получить массив пользователей попадающих в возрастную категорию от min до max лет(поле age).
 
-// const getUsersWithAge = (users, min, max) => {
-//     // твой код
-// };
+const getUsersWithAge = (users, min, max) =>
+  users
+    .filter((user) => user.age >= min && user.age <= max)
+    .map((user) => user.name);
 
-// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
-// console.log(getUsersWithAge(users, 30, 40));
+console.log(getUsersWithAge(users, 30, 40));
 // // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 // Задание 7
