1bb2cc4718ce92b439e5347b9382f395e2c25f34
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-01-23 18:10:44 +0200

    выполнено 4 задания

--- js/main.js
+++ js/main.js
@@ -58,48 +58,56 @@ console.log(getInactiveUsers(users)); // [объект Moore Hensley, объек
 
 // Получить массив пользователей попадающих в возрастную категорию от min до max лет(поле age).
 
-const getUsersWithAge = (users, min, max) =>
-  users
-    .filter((user) => user.age >= min && user.age <= max)
-    .map((user) => user.name);
+// const getUsersWithAge = (users, min, max) =>
+//   users
+//     .filter((user) => user.age >= min && user.age <= max)
+//     .map((user) => user.name);
 
-console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
-console.log(getUsersWithAge(users, 30, 40));
+// console.log(getUsersWithAge(users, 30, 40));
 // // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 // Задание 7
 
 // Получить общую сумму баланса(поле balance) всех пользователей.
 
-const calculateTotalBalance = (users) =>
-  users.reduce(function (balanced, user) {
-    return balanced + user.balance;
-  }, 0);
+// const calculateTotalBalance = (users) =>
+//   users.reduce(function (balanced, user) {
+//     return balanced + user.balance;
+//   }, 0);
 
-console.log(calculateTotalBalance(users)); // 20916
+// console.log(calculateTotalBalance(users)); // 20916
 
 // Задание 8
 
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
-// const getUsersWithFriend = (users, friendName) => {
-//     // твой код
-// };
+// const getUsersWithFriend = (users, friendName) =>
+//   users
+//     .filter(function (user) {
+//       return user.friends === friendName;
+//     })
 
-// console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+//     .map(function (user) {
+//       return user.name;
+//     });
 
-// Задание 9
+// console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// Массив имен(поле name) людей, отсортированных в зависимости от количества их друзей(поле friends)
+//Задание 9
 
-// const getNamesSortedByFriendsCount = users => {
-//     // твой код
-// };
+//Массив имен(поле name) людей, отсортированных в зависимости от количества их друзей(поле friends)
 
-// console.log(getNamesSortedByFriendsCount(users));
-// // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+// const getNamesSortedByFriendsCount = (users) =>
+
+//   const priceAscCompare = (a, b) => a.friends - b.friends;
+//   const itemsByAscendingPrice = items
+//   .slice()
+//   .sort(priceAscCompare);
+//   console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 // Задание 10
 
